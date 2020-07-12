96b4ab07052e8a7087df8c25a275d9aabb8d31a6
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-07-12 14:30:20 +0700

    remove unused code from Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import axios from "axios";
 import { Route, Switch, Link } from "react-router-dom";
 import classNames from "classnames";
 
 // components
 import Board from "./Board";
-// import Project from "./Project";
 import AddStory from "./forms/addStory";
 import Loader from "./Loader";
 import Header from "./common/Header";
-
-// action
-import {
-  getProject,
-  setLoadingProject,
-  deleteTaskRequest,
-  updateStatusTaskRequest,
-} from "../actions/index";
 
 class Dashboard extends Component {
   constructor(props, context) {
@@ -30,22 +20,8 @@ class Dashboard extends Component {
   }
 
   componentDidMount = () => {
-    // this.getStoryDetails();
     this.getProjects();
-    // setInterval(() => {
-    //   this.getData();
-    // }, 2000);
   };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // console.log("------------MATCH------------");
-    // console.log(nextProps.match.params.id);
-    // console.log(prevState.match.params.id);
-    if (nextProps.project !== prevState.project) {
-      return { project: nextProps.project };
-    }
-    return null;
-  }
 
   getProjects = () => {
     axios
@@ -59,17 +35,10 @@ class Dashboard extends Component {
         });
       })
       .catch((e) => {
-        this.props.setLoadingProject(true);
+        this.setState({
+          loading: true,
+        });
       });
-  };
-
-  handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
-    const status = targetLaneId.split("-")[1];
-    this.props.updateStatusTaskRequest(cardId, status);
-  };
-
-  onCardDelete = (cardId, laneId) => {
-    this.props.deleteTaskRequest(cardId);
   };
 
   render() {
@@ -133,17 +102,4 @@ class Dashboard extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  loadingProject: state.loading.loadingProject,
-  project: state.project,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setLoadingProject: (status) => dispatch(setLoadingProject(status)),
-  getProject: (id) => dispatch(getProject(id)),
-  deleteTaskRequest: (id) => dispatch(deleteTaskRequest(id)),
-  updateStatusTaskRequest: (id, status) =>
-    dispatch(updateStatusTaskRequest(id, status)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
