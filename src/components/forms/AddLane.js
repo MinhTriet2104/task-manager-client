3f0f71c8d3bbbf79f9ffd1a838b08ce543c3f0de
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-07-26 11:14:33 +0700

    add close button for AddLane

--- src/components/forms/AddLane.js
+++ src/components/forms/AddLane.js
@@ -1,41 +1,75 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "mdi-react/CloseIcon";
+import Typography from "@material-ui/core/Typography";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
 
 export default ({ open, handleClose }) => {
+  const styles = (theme) => ({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: "absolute",
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
+
+  const DialogTitle = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <Typography variant="h6">{children}</Typography>
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            className={classes.closeButton}
+            onClick={onClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    );
+  });
+
   return (
     <Dialog
       open={open}
       onClose={handleClose}
       aria-labelledby="form-dialog-title"
     >
-      <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+      <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+        Add New Lane
+      </DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
-        </DialogContentText>
         <TextField
           autoFocus
           margin="dense"
           id="name"
+          name="name"
           label="Email Address"
-          type="email"
+          type="text"
           fullWidth
+          required
         />
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
+        <Button onClick={handleClose} variant="contained" color="primary">
+          Save
         </Button>
-        <Button onClick={handleClose} color="primary">
-          Subscribe
+        <Button onClick={handleClose} color="secondary">
+          Cancel
         </Button>
       </DialogActions>
     </Dialog>
