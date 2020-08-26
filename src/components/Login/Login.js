f450b0b83eba1fb5083927d1b2adf1c019c5c0f1
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2020-08-26 13:09:46 +0700

    style Login Page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,46 @@
 import React from "react";
+import styled from "styled-components";
 import FacebookLogin from "react-facebook-login";
 import GoogleLogin from "react-google-login";
+
+import "../../styles/Login.scss";
+
+const LoginContainer = styled.div`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginWrapper = styled.div`
+  height: 500px;
+  width: 600px;
+
+  display: flex;
+
+  background: #fff;
+  box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.12);
+
+  border-radius: 4px;
+`;
+
+const LeftSection = styled.div`
+  width: 40%;
+
+  background-image: url("https://i.pinimg.com/564x/3c/e5/f6/3ce5f6f5ee9dba8cfb2445cbe94eae10.jpg");
+  background-size: cover;
+  background-position: center;
+`;
+
+const RightSection = styled.div`
+  width: 60%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Login = () => {
   const responseFacebook = (response) => {
@@ -12,23 +52,29 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <FacebookLogin
-        appId="661692841097516"
-        fields="name,email,picture"
-        callback={responseFacebook}
-      />
+    <LoginContainer>
+      <LoginWrapper>
+        <LeftSection></LeftSection>
+        <RightSection>
+          <FacebookLogin
+            appId="661692841097516"
+            textButton="FACEBOOK"
+            fields="name,email,picture"
+            callback={responseFacebook}
+            cssClass="fb-login-btn"
+            icon="fa-facebook"
+          />
 
-      <br />
-      <br />
-
-      <GoogleLogin
-        clientId="738277559607-qmkk4k8rared9tltub646d02oq8bvkuf.apps.googleusercontent.com"
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
-    </div>
+          <GoogleLogin
+            clientId="738277559607-qmkk4k8rared9tltub646d02oq8bvkuf.apps.googleusercontent.com"
+            buttonText="GOOGLE"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            className="gg-login-btn"
+          />
+        </RightSection>
+      </LoginWrapper>
+    </LoginContainer>
   );
 };
 
